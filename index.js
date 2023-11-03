bcb686d4da0e2f15dd84245abbbcd74442441cf9
Chad Edwards <chadedwardsofficial@gmail.com>
2023-11-03 14:26:36 -0500

    created an array of questions with assorted types of input and list using inquirer

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
+
+
+
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Provide a description of your project:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Provide installation instructions:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Provide usage information:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Provide contribution guidelines:',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Provide test instructions:',
+    name: 'tests',
+  },
+  {
+    type: 'list',
+    message: 'Choose a license for your application:',
+    name: 'license',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause'],
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+];
